add7d3cb5d77837c3fc2a2698050c03ab1eb74b6
s-bhageloe <2117940@talnet.nl>
2020-12-04 21:05:47 +0100

    Opdracht 4

--- statements.sql
+++ statements.sql
@@ -3,8 +3,13 @@ CREATE DATABASE brokenauth;
 USE brokenauth;
 
 CREATE TABLE user(
-id int NOT NULL auto_increment,
-email varchar(255) NOT NULL,
-password varchar(255) NOT NULL,
-PRIMARY KEY(id) 
+	id int NOT NULL auto_increment,
+	email varchar(255) NOT NULL,
+	password varchar(255) NOT NULL,
+	PRIMARY KEY(id) 
+);
+
+ALTER TABLE user ADD COLUMN(
+	IP_adrress VARCHAR(80),
+	datum DATETIME DEFAULT CURRENT_TIMESTAMP
 );
